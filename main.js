1be18391bc4566a588c0502961d44fdf39493b67
megaeonise <rikthmeowhumayun@gmail.com>
2021-06-30 22:07:33 +0600

    Botspam fixed somewhat

--- main.js
+++ main.js
@@ -36,9 +36,17 @@ const eightball = [
     'Very doubtful'
 ]
 
+const fatwords = [
+    'FAT',
+    'FAT PIGEON',
+    'ATTACK PIGEON',
+    'who rikth?',
+]
+
 
 function gotMessage(msg) {
     const index = Math.floor(Math.random() * eightball.length);
+    const index1 = Math.floor(Math.random() * fatwords.length);
     let message = msg.content.toLowerCase()
         if (msg.content === 't!help') {
         msg.reply('Functions = 8ball, t!ping');
@@ -46,22 +54,22 @@ function gotMessage(msg) {
     if (message.includes('test')) {
         msg.reply('dont worry, your net is working...');
     }
-    if (message.includes('some')) {
+    if (message.includes('some') && msg.author != '858413343089492009') {
         msg.channel.send('OHHHHHHH SOME REFERENCE');
     }
-    if (message.includes('shame')) {
+    if (message.includes('shame') && msg.author != '858413343089492009') {
         msg.channel.send('no shaming nothing doing im not doing anyhting im sleeping');
     }
     if (message.includes('8ball')) {
         msg.reply(eightball[index]);
         }
-    if (msg.content === '!DEBUGMESSAGE1') {
+    if (msg.content === '!DEBUGMESSAGE1' && msg.author != '858413343089492009') {
         msg.channel.send(message);
     }
     if (msg.content === 't!ping') {
         msg.channel.send('pong');
     }
-    if (message.includes('full on')) {
+    if (message.includes('full on') && msg.author != '858413343089492009') {
         msg.channel.send('ayy half some');
     }
     if (message.includes === 'ennada shaniyane onnu vecchena eppudi irkum teriyamo' && msg.author != '858413343089492009') {
@@ -73,5 +81,8 @@ function gotMessage(msg) {
     if (message.includes('ay') && message.includes('megaeonise') && msg.author != '858413343089492009') {
         msg.channel.send('ay megaeonise');
     }
+    if (message.includes('fat') && msg.author != '858413343089492009') {
+        msg.reply(fatwords[index1]);
+        }
 }
 
