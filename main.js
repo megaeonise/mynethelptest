682a728f82980b1727dca82b089dc5206383aed5
megaeonise <rikthmeowhumayun@gmail.com>
2021-07-12 23:51:42 +0600

    attempting to add shutdown

--- main.js
+++ main.js
@@ -50,6 +50,8 @@ const ashfaroth = [
     'ayy bhalu jaisa bhalu',
 ]
 
+const canshutdown = ['190756291558375424']
+
 
 function gotMessage(msg) {
     const index = Math.floor(Math.random() * eightball.length);
@@ -119,6 +121,9 @@ function gotMessage(msg) {
         }
         if ((message.includes('baal'))) {
             msg.channel.send('https://cdn.discordapp.com/attachments/777823278567915522/863454029359349780/unknown.png');
+        }
+        if (message.includes('!!exit') && msg.author('' in canshutdown)) {
+            exit();
         }
     }
 }
